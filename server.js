f734538bb97633a32dfddf0ac19876e76fc317f0
Naveen Kumar <1236naveensingh@gmail.com>
2026-05-21 09:56:00 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// Home Route
+app.get("/", (req, res) => {
+
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+
+});
+
+// Login Route
+app.post("/login", (req, res) => {
+
+    const { email, password } = req.body;
+
+    console.log("Email:", email);
+    console.log("Password:", password);
+
+    res.json({
+
+        success: true,
+        message: "Data Received Successfully"
+
+    });
+
+});
+
+app.listen(PORT, () => {
+
+    console.log(`Server Running On http://localhost:${PORT}`);
+
+});
